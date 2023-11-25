cd4387eb3618d8b0919d417bd9263cae0d3a38a5
agusmena <agusmena60@mail.com>
2023-11-25 13:53:18 -0300

    configuration express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("desde el backend")
+import express from 'express';
+
+const app = express();
+app.set('port', process.env.PORT || 4001)
+app.listen(app.get('port'), ()=>(
+    console.log('estoy en el puerto ' + app.get('port'))
+))
