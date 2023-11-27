9101aba2b51118eac978482e455ca8ea99e79f90
agusmena <agusmena60@mail.com>
2023-11-27 12:06:34 -0300

    change db connection

--- src/database.js
+++ src/database.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
-const url ='mongodb://127.0.0.1:27017/veterinaria'
-mongoose.connect(url);
+import 'dotenv/config'
+//const url ='mongodb://127.0.0.1:27017/veterinaria'
+//mongoose.connect(url);
+
+const URI= process.env.MONGODB_URI
+mongoose.connect(URI)
+
 const connection = mongoose.connection
 connection.once('open', ()=>{
     console.log("Data base connected!")
