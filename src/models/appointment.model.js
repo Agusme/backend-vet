6911ad24a410e9f8a38c80173326487f4540c9ec
agusmena <agusmena60@mail.com>
2023-12-04 16:10:30 -0300

    add changes in appointment

--- src/models/appointment.model.js
+++ src/models/appointment.model.js
@@ -24,14 +24,16 @@ const appointmentSchema = new Schema({
         require: true,
         type: String,
     },
-    time:{
+    time: {
         require: true,
-        type:String,
-        min: 8,
-        max:16,
-
-    }
-})
+        type: String,
+        validate: {
+          validator: (value) => /^(?:1[0-6]|0?[8-9]):[0-5][0-9]$/.test(value),
+          message: 'La hora debe estar en formato de 24 horas (por ejemplo, "14:30").',
+        },
+      },
+    });
+    
 
 const Appointment = mongoose.model('appointment', appointmentSchema);
 export default Appointment;
