a0e5d877aec176584832bf3d56127fda929c9580
agusmena <agusmena60@mail.com>
2023-12-04 16:17:40 -0300

    add changes in appointment model

--- src/models/appointment.model.js
+++ src/models/appointment.model.js
@@ -1,39 +1,37 @@
-import { text } from "express";
-import mongoose,{Schema} from "mongoose";
+import { Schema } from "mongoose";
 
 const appointmentSchema = new Schema({
-    appointmentDetail:{
-        require: true,
-        type:String,
-        minlenght: 2,
-        maxlenght:30,
+    appointmentDetail: {
+        required: true,
+        type: String,
+        minlength: 2,
+        maxlength: 30,
     },
-    veterinarian:{
-        require: true,
-        type:String,
-        minlenght: 2,
-        maxlenght:30,
+    veterinarian: {
+        required: true,
+        type: String,
+        minlength: 2,
+        maxlength: 30,
     },
-    pet:{
-        require: true,
-        type:String,
-        minlenght: 2,
-        maxlenght:30,
+    pet: {
+        required: true,
+        type: String,
+        minlength: 2,
+        maxlength: 30,
     },
-    date:{
-        require: true,
+    date: {
+        required: true,
         type: String,
     },
     time: {
-        require: true,
+        required: true,
         type: String,
         validate: {
-          validator: (value) => /^(?:1[0-6]|0?[8-9]):[0-5][0-9]$/.test(value),
-          message: 'La hora debe estar en formato de 24 horas (por ejemplo, "14:30").',
+            validator: (value) => /^(?:1[0-6]|0?[8-9]):[0-5][0-9]$/.test(value),
+            message: 'La hora debe estar en formato de 24 horas (por ejemplo, "14:30").',
         },
-      },
-    });
-    
+    },
+});
 
 const Appointment = mongoose.model('appointment', appointmentSchema);
 export default Appointment;
