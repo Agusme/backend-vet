dce8686973576b5af424639b150a3fe04f3a079e
maxigiordani <maxigiordani05@gmail.com>
2023-12-12 11:49:24 -0300

    deleted coments

--- src/database.js
+++ src/database.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
-//const url ='mongodb://127.0.0.1:27017/veterinaria'
-//mongoose.connect(url);
 
 const URI= process.env.MONGODB_URI
 mongoose.connect(URI)
